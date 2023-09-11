f9fe094fc27fb2824b7508d7b1535df814f73098
M2.Developer <35066105+M2D3V@users.noreply.github.com>
2023-09-12 03:04:49 +0700

    Release
    Start Project M2.Developer For FiveM Script

---
+++ M2Checker/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'M2.Developer <M2Dev>'
+name '^4Self Check'
+description 'M2.Developer : Script Checker'
+
+version '1.0.0.0'
+
+server_script 'service.lua'
+client_script 'client.lua'
+
+M2Checker 'yes'
